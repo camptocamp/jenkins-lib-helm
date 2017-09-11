43f3e0d8103d5c6f466ea60058e4439f4dbffefd
Marc Sutter <marc.sutter@swisscom.com>
2017-09-11 12:52:55 +0200

    try to escape quotes

--- src/com/camptocamp/Helm.groovy
+++ src/com/camptocamp/Helm.groovy
@@ -78,11 +78,11 @@ def helmDeploy(Map args) {
     if (args.dry_run) {
         println "Running dry-run deployment"
 
-        sh $/helm upgrade --dry-run --install ${args.name} ${args.chart_dir} $values --namespace=${namespace} --tiller-namespace=${tiller_namespace}/$
+        sh $/helm upgrade --dry-run --install $args.name $args.chart_dir $values --namespace=$namespace --tiller-namespace=$tiller_namespace/$
     } else {
         println "Running deployment"
 
-        sh $/helm upgrade --wait --install ${args.name} ${args.chart_dir} $values --namespace=${namespace} --tiller-namespace=${tiller_namespace}/$
+        sh $/helm upgrade --wait --install $args.name $args.chart_dir $values --namespace=$namespace --tiller-namespace=$tiller_namespace/$
 
         echo "Application ${args.name} successfully deployed. Use helm status ${args.name} to check"
     }
